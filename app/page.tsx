66125e67be2d3ee8e4421e61f1fa65b5b161ee2d
Sumile Turki <turkisumile@gmail.com>
2025-11-13 20:01:50 +0530

    trpc-setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-import prisma from "@/lib/db";
+"use client";
 import  {cn} from "@/lib/utils"
-export default async function Home() {
-  const user = await prisma.user.findMany();
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+const Page =()=> {
+  const trpc = useTRPC();
+  const {data: user} = useQuery(trpc.getUsers.queryOptions())
   return (
     <div className="flex text-red-300 min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <div className={cn("text-2xl")}>
@@ -11,3 +14,5 @@ export default async function Home() {
     </div>
   );
 }
+
+export default Page;  
